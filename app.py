dc806632ad3c542ff9d0982ec48058075b6d12ce
John Meow <mr.kofe.edk@ya.ru>
2019-05-19 20:58:28 +0300

    T

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def q0():
         session['answers'] = 0
     return render_template('question.html',
                            next=1,
-                           question='По какому произведению Вы проходите викторину? Кто автор произведения? $ echo Test4 > /dev/null/',
+                           question='По какому произведению Вы проходите викторину? Кто автор произведения? $ echo Test5 > /dev/null/',
                            hints=[
                                'Название произведения',
                                'Автор'
